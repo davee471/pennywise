d908dfd426e2f2d66349567a0d90b4f870695f31
unknown <ar.sameh1106@gmail.com>
2026-04-26 22:07:03 +0300

    Set-up dbManager expect

--- composeApp/src/commonMain/kotlin/stud/brokers/pennywise/models/DatabaseManager.kt
+++ composeApp/src/commonMain/kotlin/stud/brokers/pennywise/models/DatabaseManager.kt
@@ -1,4 +1,11 @@
 package stud.brokers.pennywise.models
 
 expect object DatabaseManager {
+    fun saveCycle(cycle: BudgetCycle): Boolean
+    fun saveTransaction(tx: Transaction): Boolean
+    fun saveCategory(cat: Category): Boolean
+    fun fetchTransactions(): List<Transaction>
+    fun fetchCycle(): BudgetCycle?
+    fun fetchCategories(): List<Category>
+
 }
