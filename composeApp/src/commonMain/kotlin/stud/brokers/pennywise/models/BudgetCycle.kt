5cf493ec1db0df3fe7479dec6a63c6cd269ba253
unknown <ar.sameh1106@gmail.com>
2026-04-26 17:36:00 +0300

    Implemented budgetcycle class

--- composeApp/src/commonMain/kotlin/stud/brokers/pennywise/models/BudgetCycle.kt
+++ composeApp/src/commonMain/kotlin/stud/brokers/pennywise/models/BudgetCycle.kt
@@ -1,3 +1,23 @@
 package stud.brokers.pennywise.models
 
-data class BudgetCycle()
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.daysUntil
+import kotlinx.datetime.toLocalDateTime
+import kotlin.time.Clock
+
+data class BudgetCycle(
+    var totalAllowance: Double,
+    val startDate: LocalDate,
+    val endDate: LocalDate,
+){
+    val totalDays: Int
+        get() = startDate.daysUntil(endDate)
+
+    val remainingDays: Int
+        get() {
+            val today = Clock.System.now()
+                .toLocalDateTime(TimeZone.currentSystemDefault()).date
+            return today.daysUntil(endDate).coerceAtLeast(1)
+        }
+}
