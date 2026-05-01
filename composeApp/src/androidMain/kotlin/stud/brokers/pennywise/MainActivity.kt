54cca237c375b6858d8326cf5692b1d7421a86f6
davee471 <davidmofreh471@gmail.com>
2026-05-01 12:48:09 +0300

    a commit msg idk

--- composeApp/src/androidMain/kotlin/stud/brokers/pennywise/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/stud/brokers/pennywise/MainActivity.kt
@@ -4,22 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import stud.brokers.pennywise.controllers.BudgetController
+import stud.brokers.pennywise.controllers.SettingsController
+import stud.brokers.pennywise.db.DatabaseManager
+import stud.brokers.pennywise.db.DriverFactory
+import stud.brokers.pennywise.services.ExportService
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
+        // Build the controllers
+        val driverFactory = DriverFactory(this)
+        val exportService = ExportService()
+        val dbManager = DatabaseManager(driverFactory)
+        val budgetController = BudgetController(dbManager)
+        val settingsController = SettingsController(dbManager, budgetController, exportService)
+
         setContent {
-            App()
+            // Pass it in!
+            App(settingsController = settingsController)
         }
     }
-}
-
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
 }
--- composeApp/src/jvmMain/kotlin/stud/brokers/pennywise/main.kt
+++ composeApp/src/jvmMain/kotlin/stud/brokers/pennywise/main.kt
@@ -2,12 +2,25 @@ package stud.brokers.pennywise
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import stud.brokers.pennywise.controllers.BudgetController
+import stud.brokers.pennywise.controllers.SettingsController
+import stud.brokers.pennywise.db.DatabaseManager
+import stud.brokers.pennywise.db.DriverFactory
+import stud.brokers.pennywise.services.ExportService
 
 fun main() = application {
+    // Build the controllers
+    val driverFactory = DriverFactory()
+    val exportService = ExportService()
+    val dbManager = DatabaseManager(driverFactory)
+    val budgetController = BudgetController(dbManager)
+    val settingsController = SettingsController(dbManager, budgetController, exportService)
+
     Window(
         onCloseRequest = ::exitApplication,
         title = "PennyWise",
     ) {
-        App()
+        // Pass it in!
+        App(settingsController = settingsController)
     }
 }
