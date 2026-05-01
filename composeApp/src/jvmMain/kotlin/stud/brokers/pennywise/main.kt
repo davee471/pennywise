1ca7955777795954fe51f4d8250800daa217b592
unknown <ar.sameh1106@gmail.com>
2026-05-02 02:12:53 +0300

    fixed buggggggg

--- composeApp/src/jvmMain/kotlin/stud/brokers/pennywise/main.kt
+++ composeApp/src/jvmMain/kotlin/stud/brokers/pennywise/main.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import stud.brokers.pennywise.controllers.BudgetController
 import stud.brokers.pennywise.controllers.SettingsController
+import stud.brokers.pennywise.controllers.TransactionController
 import stud.brokers.pennywise.db.DatabaseManager
 import stud.brokers.pennywise.db.DriverFactory
 import stud.brokers.pennywise.services.ExportService
@@ -13,7 +14,8 @@ fun main() = application {
     val driverFactory = DriverFactory()
     val exportService = ExportService()
     val dbManager = DatabaseManager(driverFactory)
-    val budgetController = BudgetController(dbManager)
+    val txController = TransactionController(dbManager)
+    val budgetController = BudgetController(dbManager, txController = txController)
     val settingsController = SettingsController(dbManager, budgetController, exportService)
 
     Window(
